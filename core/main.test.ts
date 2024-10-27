b0d58b1e371313e008c9c0d4ffa83c658e51fd7c
Christopher Duncan <chris.duncan.arauz+git@protonmail.com>
2024-10-26 23:18:32 -0700

    Initialize a new Salem game

---
+++ core/main.test.ts
@@ -0,0 +1,206 @@
+import { describe, it } from "jsr:@std/testing/bdd";
+import { expect } from "jsr:@std/expect";
+import { Either } from "npm:effect";
+
+import {
+  initializeGame,
+  TooFewPlayersError,
+  TooManyPlayersError,
+} from "./main.ts";
+
+describe("#initializeGame", () => {
+  it("should return an error if there are too few players", () => {
+    const result = initializeGame(["Alice"]);
+
+    expect(result).toEqual(Either.left(new TooFewPlayersError(1)));
+  });
+
+  it("should give five cards to each player if there are four or more players", () => {
+    const result = initializeGame(["Alice", "Bob", "Charlie", "David"]);
+
+    expect(result).toEqual(
+      Either.right({
+        players: [
+          { position: 0, name: "Alice", cardAmount: 5 },
+          { position: 1, name: "Bob", cardAmount: 5 },
+          { position: 2, name: "Charlie", cardAmount: 5 },
+          { position: 3, name: "David", cardAmount: 5 },
+        ],
+        trialCardsRevealed: 0,
+      })
+    );
+  });
+
+  it("should give five cards to each player if there are six or less players", () => {
+    const result = initializeGame([
+      "Alice",
+      "Bob",
+      "Charlie",
+      "David",
+      "Eve",
+      "Frank",
+    ]);
+
+    expect(result).toEqual(
+      Either.right({
+        players: [
+          { position: 0, name: "Alice", cardAmount: 5 },
+          { position: 1, name: "Bob", cardAmount: 5 },
+          { position: 2, name: "Charlie", cardAmount: 5 },
+          { position: 3, name: "David", cardAmount: 5 },
+          { position: 4, name: "Eve", cardAmount: 5 },
+          { position: 5, name: "Frank", cardAmount: 5 },
+        ],
+        trialCardsRevealed: 0,
+      })
+    );
+  });
+
+  it("should give four cards to each player if there are seven or more players", () => {
+    const result = initializeGame([
+      "Alice",
+      "Bob",
+      "Charlie",
+      "David",
+      "Eve",
+      "Frank",
+      "Grace",
+    ]);
+
+    expect(result).toEqual(
+      Either.right({
+        players: [
+          { position: 0, name: "Alice", cardAmount: 4 },
+          { position: 1, name: "Bob", cardAmount: 4 },
+          { position: 2, name: "Charlie", cardAmount: 4 },
+          { position: 3, name: "David", cardAmount: 4 },
+          { position: 4, name: "Eve", cardAmount: 4 },
+          { position: 5, name: "Frank", cardAmount: 4 },
+          { position: 6, name: "Grace", cardAmount: 4 },
+        ],
+        trialCardsRevealed: 0,
+      })
+    );
+  });
+
+  it("should give four cards to each player if there are nine or less players", () => {
+    const result = initializeGame([
+      "Alice",
+      "Bob",
+      "Charlie",
+      "David",
+      "Eve",
+      "Frank",
+      "Grace",
+      "Heidi",
+      "Ivan",
+    ]);
+
+    expect(result).toEqual(
+      Either.right({
+        players: [
+          { position: 0, name: "Alice", cardAmount: 4 },
+          { position: 1, name: "Bob", cardAmount: 4 },
+          { position: 2, name: "Charlie", cardAmount: 4 },
+          { position: 3, name: "David", cardAmount: 4 },
+          { position: 4, name: "Eve", cardAmount: 4 },
+          { position: 5, name: "Frank", cardAmount: 4 },
+          { position: 6, name: "Grace", cardAmount: 4 },
+          { position: 7, name: "Heidi", cardAmount: 4 },
+          { position: 8, name: "Ivan", cardAmount: 4 },
+        ],
+        trialCardsRevealed: 0,
+      })
+    );
+  });
+
+  it("should give three cards to each player if there are ten or more players", () => {
+    const result = initializeGame([
+      "Alice",
+      "Bob",
+      "Charlie",
+      "David",
+      "Eve",
+      "Frank",
+      "Grace",
+      "Heidi",
+      "Ivan",
+      "Judy",
+    ]);
+
+    expect(result).toEqual(
+      Either.right({
+        players: [
+          { position: 0, name: "Alice", cardAmount: 3 },
+          { position: 1, name: "Bob", cardAmount: 3 },
+          { position: 2, name: "Charlie", cardAmount: 3 },
+          { position: 3, name: "David", cardAmount: 3 },
+          { position: 4, name: "Eve", cardAmount: 3 },
+          { position: 5, name: "Frank", cardAmount: 3 },
+          { position: 6, name: "Grace", cardAmount: 3 },
+          { position: 7, name: "Heidi", cardAmount: 3 },
+          { position: 8, name: "Ivan", cardAmount: 3 },
+          { position: 9, name: "Judy", cardAmount: 3 },
+        ],
+        trialCardsRevealed: 0,
+      })
+    );
+  });
+
+  it("should give three cards to each player if there are twelve or less players", () => {
+    const result = initializeGame([
+      "Alice",
+      "Bob",
+      "Charlie",
+      "David",
+      "Eve",
+      "Frank",
+      "Grace",
+      "Heidi",
+      "Ivan",
+      "Judy",
+      "Kevin",
+      "Laura",
+    ]);
+
+    expect(result).toEqual(
+      Either.right({
+        players: [
+          { position: 0, name: "Alice", cardAmount: 3 },
+          { position: 1, name: "Bob", cardAmount: 3 },
+          { position: 2, name: "Charlie", cardAmount: 3 },
+          { position: 3, name: "David", cardAmount: 3 },
+          { position: 4, name: "Eve", cardAmount: 3 },
+          { position: 5, name: "Frank", cardAmount: 3 },
+          { position: 6, name: "Grace", cardAmount: 3 },
+          { position: 7, name: "Heidi", cardAmount: 3 },
+          { position: 8, name: "Ivan", cardAmount: 3 },
+          { position: 9, name: "Judy", cardAmount: 3 },
+          { position: 10, name: "Kevin", cardAmount: 3 },
+          { position: 11, name: "Laura", cardAmount: 3 },
+        ],
+        trialCardsRevealed: 0,
+      })
+    );
+  });
+
+  it("should return an error if there are too many players", () => {
+    const result = initializeGame([
+      "Alice",
+      "Bob",
+      "Charlie",
+      "David",
+      "Eve",
+      "Frank",
+      "Grace",
+      "Heidi",
+      "Ivan",
+      "Judy",
+      "Kevin",
+      "Laura",
+      "Mallory",
+    ]);
+
+    expect(result).toEqual(Either.left(new TooManyPlayersError(13)));
+  });
+});
---
+++ core/main.ts
@@ -0,0 +1,90 @@
+import { Either } from "npm:effect";
+
+const MINIMUM_AMOUNT_PLAYERS = 4;
+const MAXIMUM_AMOUNT_PLAYERS = 12;
+
+interface GameInfo {
+  players: PlayerInfo[];
+  trialCardsRevealed: number;
+}
+
+interface PlayerInfo {
+  position: number;
+  name: string;
+  cardAmount: number;
+}
+
+export class TooFewPlayersError {
+  readonly _tag = "TooFewPlayersError";
+  constructor(readonly playerAmount: number) {}
+}
+
+export class TooManyPlayersError {
+  readonly _tag = "TooManyPlayersError";
+  constructor(readonly playerAmount: number) {}
+}
+
+export function initializeGame(
+  playerNames: string[]
+): Either.Either<GameInfo, TooFewPlayersError | TooManyPlayersError> {
+  return Either.gen(function* () {
+    const validNumberOfPlayers = yield* parseValidNumberOfPlayers(
+      playerNames.length
+    );
+    const cardAmount =
+      initialCardAmountPerNumberOfPlayers[validNumberOfPlayers];
+
+    return {
+      players: playerNames.map((name, position) => ({
+        position,
+        name,
+        cardAmount,
+      })),
+      trialCardsRevealed: 0,
+    };
+  });
+}
+
+enum ValidNumberOfPlayers {
+  Four = 4,
+  Five = 5,
+  Six = 6,
+  Seven = 7,
+  Eight = 8,
+  Nine = 9,
+  Ten = 10,
+  Eleven = 11,
+  Twelve = 12,
+}
+
+function parseValidNumberOfPlayers(
+  amountOfPlayers: number
+): Either.Either<
+  ValidNumberOfPlayers,
+  TooFewPlayersError | TooManyPlayersError
+> {
+  if (amountOfPlayers < MINIMUM_AMOUNT_PLAYERS) {
+    return Either.left(new TooFewPlayersError(amountOfPlayers));
+  }
+
+  if (amountOfPlayers > MAXIMUM_AMOUNT_PLAYERS) {
+    return Either.left(new TooManyPlayersError(amountOfPlayers));
+  }
+
+  return Either.right(amountOfPlayers);
+}
+
+const initialCardAmountPerNumberOfPlayers: Record<
+  ValidNumberOfPlayers,
+  number
+> = {
+  [ValidNumberOfPlayers.Four]: 5,
+  [ValidNumberOfPlayers.Five]: 5,
+  [ValidNumberOfPlayers.Six]: 5,
+  [ValidNumberOfPlayers.Seven]: 4,
+  [ValidNumberOfPlayers.Eight]: 4,
+  [ValidNumberOfPlayers.Nine]: 4,
+  [ValidNumberOfPlayers.Ten]: 3,
+  [ValidNumberOfPlayers.Eleven]: 3,
+  [ValidNumberOfPlayers.Twelve]: 3,
+};
